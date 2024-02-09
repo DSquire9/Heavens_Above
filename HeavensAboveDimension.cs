863b649dd28b56af16f79ea34fd04fa0d946eba0
samercer2002 <70397624+samercer2002@users.noreply.github.com>
2024-02-09 18:20:57 -0500

    Added basic single island generation
    Removed half dirt world generation and added a single island generation instead. Treating islands as a "structure" rather than a noise-based entity generation.

--- HeavensAboveDimension.cs
+++ HeavensAboveDimension.cs
@@ -6,6 +6,7 @@ using Terraria.IO;
 using Terraria.ModLoader;
 using Terraria;
 using Terraria.WorldBuilding;
+using HeavensAbove.Structures;
 
 namespace Heavens_Above
 {
@@ -24,7 +25,8 @@ namespace Heavens_Above
 
         public override List<GenPass> Tasks => new List<GenPass>()
     {
-        new WorldGenPass()
+        new WorldGenPass(),
+        new IslandGenPass()
     };
     }
 
@@ -40,19 +42,31 @@ namespace Heavens_Above
             Main.worldSurface = Main.maxTilesY - 42; // Hides the underground layer just out of bounds
             Main.rockLayer = Main.maxTilesY + 100; // Hides the cavern layer way out of bounds
 
-            for (int i = 0; i < Main.maxTilesX; i++)
+/*            for (int i = 0; i < Main.maxTilesX; i++)
             {
-                for (int j = Main.maxTilesY / 2 + 1; j < Main.maxTilesY; j++)
+                for (int j = Main.maxTilesY / 2+2; j < Main.maxTilesY; j++)
                 {
                     progress.Set((j + i * Main.maxTilesY) / (float)(Main.maxTilesX * Main.maxTilesY)); // Controls the progress bar, should only be set between 0f and 1f
                     Tile tile = Main.tile[i, j];
                     tile.HasTile = true;
                     tile.TileType = TileID.Dirt;
                 }
-            }
+            }*/
+
         }
     }
 
+    public class IslandGenPass : GenPass
+    {
+        public IslandGenPass() : base("Islands", 1) { }
+
+        protected override void ApplyPass(GenerationProgress progress, GameConfiguration configuration)
+        {
+            progress.Message = "Generating Islands";
+            Island isl = new Island();
+            isl.Generate(false);
+        }
+    }
     public class UpdateSubworldSystem : ModSystem
     {
         public override void PreUpdateWorld()
