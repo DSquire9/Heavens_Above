68721fa7ce1698266c828d8030549551550b75b8
DSquire9 <dss9593@rit.edu>
2024-03-03 16:46:04 -0500

    Added boss files

---
+++ Content/Bosses/ValkyrieQueen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace HeavensAbove.Content.Bosses
+{
+    public class ValkyrieQueen : ModNPC
+    {
+    }
+}
